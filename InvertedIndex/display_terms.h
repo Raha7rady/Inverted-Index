e8cbac70682cf52c777b6bdaae041e6987534b82
Sanaz.darbandi <mahta17rw@gmail.com>
2025-11-26 01:35:28 +0330

    Update display_terms.h

--- InvertedIndex/display_terms.h
+++ InvertedIndex/display_terms.h
@@ -1,30 +1,3 @@
-﻿//#pragma once
-//#include <map>
-//#include <vector>
-//#include <string>
-//#include <unordered_map>  // ⚠ اضافه شد
-//
-///*
-// * Function: buildSortedIndex
-// * --------------------------
-// * تبدیل invertedIndex (unordered_map) به map مرتب
-// * map در C++ از Red-Black Tree استفاده می‌کند که یک درخت متوازن شبیه B-Tree است
-// */
-//std::map<std::string, std::vector<int>>
-//buildSortedIndex(const std::unordered_map<std::string, std::vector<int>>& invertedIndex);
-//
-///*
-// * Function: printBTreeGraphical
-// * -----------------------------
-// * نمایش گرافیکی B-Tree (Simulation)
-// * - sortedIndex: ترم‌های مرتب (از map)
-// * - nodeSize: حداکثر تعداد کلید در هر گره
-// */
-//void printBTreeGraphical(const std::map<std::string, std::vector<int>>& sortedIndex,
-//    size_t nodeSize = 3);
-
-
-
 #pragma once
 #include <map>
 #include <vector>
